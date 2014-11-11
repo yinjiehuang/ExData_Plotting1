e09eb644e5d32dab526da5759c4570857700f84e
darrenhuang22 <darrenhuang22@gmail.com>
2014-11-10 22:10:58 -0500

    first commit

--- plot2.R
+++ plot2.R
@@ -1,7 +1,7 @@
 ## First of all, read the data into memory
 ## Most of the data is uncessary, we only read 01/02/2007 to 02/02/2007 data which starts from 
 ## line 66637 to line 69517
-mydata <- mydata <- read.table("household_power_consumption.txt", header = T, skip = 66636, sep = ";", nrow = 69517 - 66637)
+mydata <- read.table("household_power_consumption.txt", header = T, skip = 66636, sep = ";", nrow = 69517 - 66637)
 # Change the column name
 colnames(mydata) <- c("Date","Time","Global_active_power","Glocal_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 
